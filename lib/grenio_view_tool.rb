292fa9b9aa07ebd140e508c5792286e05b40bcc7
Marcin Gren <marrcin.gren@gmail.com>
2019-02-14 00:39:08 +0100

    Grenio tool initial commit

---
+++ lib/grenio_view_tool.rb
@@ -0,0 +1,7 @@
+require "grenio_view_tool/version"
+require "grenio_view_tool/version/renderer"
+
+module GrenioViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/grenio_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GrenioViewTool
+  VERSION = "0.1.0"
+end
